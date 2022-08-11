33d26a7d6c1514e30658a0dfa60ce17ed99d2464
themuzic <masta_jk@naver.com>
2022-08-11 13:06:31 +0900

    update

--- components/Seo.js
+++ components/Seo.js
@@ -4,6 +4,9 @@ export default function Seo({ title }) {
   return (
     <Head>
       <title>{title} | Movies & TV</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet="utf-8"></meta>
+      <meta name="author" content="Jay"></meta>
     </Head>
   );
 }
